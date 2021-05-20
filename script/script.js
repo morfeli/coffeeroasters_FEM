de04f2abbc195f1ceb830955b8229392df6bf621
Felipe Moreira <fmoreira95@hotmail.com>
2021-05-20 02:42:34 -0400

    steps container js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 const productBox = document.getElementById("products__container");
 const benefitsBox = document.getElementById("benefits__container");
+const stepsBox = document.getElementById("steps__container");
 
 let products = [
   {
@@ -49,6 +50,27 @@ let benefits = [
   },
 ];
 
+let steps = [
+  {
+    number: "01",
+    title: "Pick your coffee",
+    description:
+      "Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices for them. There are new coffees in all profiles every month for you to try out.",
+  },
+  {
+    number: "02",
+    title: "Choose the frequency",
+    description:
+      "Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.",
+  },
+  {
+    number: "03",
+    title: "Receive and enjoy!",
+    description:
+      "We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.",
+  },
+];
+
 const renderProducts = (arr) => {
   const products = arr
     .map((product) => {
@@ -82,3 +104,20 @@ const renderBenefits = (arr) => {
   benefitsBox.innerHTML = benefits;
 };
 renderBenefits(benefits);
+
+const renderSteps = (arr) => {
+  const steps = arr
+    .map((step) => {
+      return `
+            <div class="step__container">
+            <h1>${step.number}</h1>
+            <h2 class="step__container--title">${step.title}</h2>
+            <p class="step__container--description">${step.description}</p>
+            </div>
+            `;
+    })
+    .join("");
+
+  stepsBox.innerHTML = steps;
+};
+renderSteps(steps);
